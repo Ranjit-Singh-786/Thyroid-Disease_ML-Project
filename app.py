56775bfa1bb9c48babcad0a9500c696918d9b779
Ranjit singh <jiradhey402@gmail.com>
2022-12-22 00:06:13 +0530

    model buil and Api creation start

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask,render_template,url_for,request
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
