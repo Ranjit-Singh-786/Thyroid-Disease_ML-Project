3ad32918480c3e9a0d1ee7ee2781fc1b08356eb2
Ranjit singh <jiradhey402@gmail.com>
2022-12-22 23:31:19 +0530

    API creation complete

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,url_for,request,jsonify
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/predict')
+def predict():
+    return render_template('home.html')
+
+@app.route('/result' , methods=['POST'])
+def result():
+    if request.method == 'POST':
+        return render_template('prediction.html' ,pred = str(786) )
+        # return jsonify(str('welcome'))
 
 
 if __name__ =="__main__":
